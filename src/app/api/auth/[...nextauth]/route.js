6c29e243aa85a3937ac2b82f2da1fbf9d6e15511
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-02-22 17:34:44 +0600

    Create Experimental Login Logic for Credentials

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,23 +1,55 @@
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const userList = [
+  { name: "hablu", password: "1234", secretCode: "1111" },
+  { name: "dablu", password: "5678", secretCode: "2222" },
+  { name: "bablu", password: "0000", secretCode: "3333" },
+];
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
-
-      name: "Credentials",
+      name: "Email and Password",
 
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "enter Name",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "enter Password",
+        },
+        secretCode: {
+          label: "secret Code",
+          type: "number",
+          placeholder: "enter Code",
+        },
       },
       async authorize(credentials, req) {
         // login logic here----------->
+        const { username, password, secretCode } = credentials;
 
+        const user = userList.find(u=> u.name == username)
+        if(!user) return null;
+
+        const isPasswordCorrect = user.password == password;
+        if(!isPasswordCorrect) return null;
+
+        const isSecretCorrect = user.secretCode == secretCode;
+        if (isSecretCorrect) {
+            return user
+        }
         return null;
       },
     }),
   ],
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,15 @@
+"use client"
+import { signIn } from "next-auth/react"
+import React from 'react';
+
+const LoginButton = () => {
+    return (
+        <button 
+        onClick={()=> signIn()}
+        className='btn'>
+            Login Now
+        </button>
+    );
+};
+
+export default LoginButton;
