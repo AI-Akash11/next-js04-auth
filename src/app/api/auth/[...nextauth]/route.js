81fc0cf5dc5043c6d311c99e2f1418e5e82f7ded
Ali Imam Akash <akash1437aliimam@gmail.com>
2026-02-25 16:54:28 +0600

    Github SignIn , Save user to DB and Code Refactoring

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,6 @@
-import { dbConnect } from "@/lib/dbConnect";
+import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import GoogleProvider from "next-auth/providers/google";
+
 
 const userList = [
   { name: "hablu", password: "1234", secretCode: "1111" },
@@ -10,67 +8,7 @@ const userList = [
   { name: "bablu", password: "0000", secretCode: "3333" },
 ];
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    CredentialsProvider({
-      name: "Email and Password",
 
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "enter Email",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "enter Password",
-        },
-      },
-      async authorize(credentials, req) {
-        // login logic here----------->
-        const { email, password } = credentials;
-
-        // const user = userList.find(u=> u.name == username)
-
-        const user = await dbConnect("users").findOne({ email });
-        if (!user) return null;
-
-        const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-        if (isPasswordCorrect) {
-          return user;
-        }
-        return null;
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      return true;
-    },
-    //   async redirect({ url, baseUrl }) {
-    //     return baseUrl
-    //   },
-    async session({ session, token, user }) {
-      if (token) {
-        session.role = token.role;
-      }
-      return session;
-    },
-    async jwt({ token, user, account, profile, isNewUser }) {
-      if (user) {
-        ((token.email = user.email), (token.role = user.role));
-      }
-      return token;
-    },
-  },
-};
 
 const handler = NextAuth(authOptions);
 
---
+++ src/lib/authOptions.js
@@ -0,0 +1,92 @@
+import { dbConnect } from "@/lib/dbConnect";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      name: "Email and Password",
+
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "enter Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "enter Password",
+        },
+      },
+      async authorize(credentials, req) {
+        // login logic here----------->
+        const { email, password } = credentials;
+
+        // const user = userList.find(u=> u.name == username)
+
+        const user = await dbConnect("users").findOne({ email });
+        if (!user) return null;
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if (isPasswordCorrect) {
+          return user;
+        }
+        return null;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        const payload = {
+          ...user,
+          provider: account.provider,
+          providerId: account.providerAccountId,
+          role: "user",
+          createdAt: new Date().toISOString(),
+        };
+
+        if (!user?.email) {
+          return false;
+        }
+
+        const isExist = await dbConnect("users").findOne({ email: user.email, providerId: account.providerAccountId });
+        if (!isExist) {
+          const result = await dbConnect("users").insertOne(payload);
+        }
+
+        return true;
+      } catch (error) {
+        return false
+      }
+    },
+    //   async redirect({ url, baseUrl }) {
+    //     return baseUrl
+    //   },
+    async session({ session, token, user }) {
+      if (token) {
+        session.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        ((token.email = user.email), (token.role = user.role));
+      }
+      return token;
+    },
+  },
+};
